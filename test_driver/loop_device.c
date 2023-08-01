3f8b8e22429fbce352b2b679b98e63e71b646880
Khachatur Avetisyan <72801992+KhachaturAvetisyan@users.noreply.github.com>
2023-08-01 18:37:47 +0400

    change main logic in loop_device

--- test_driver/loop_device.c
+++ test_driver/loop_device.c
@@ -3,6 +3,8 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/cdev.h> 	
 
 #define LOOP_MAJOR 0
 #define LOOP_NAME "loop_device"
@@ -10,12 +12,18 @@
 
 MODULE_LICENSE("GPL");
 
+int major = 0;
+
+// Character device structure
+static struct cdev my_cdev;
+
 static char *output_file = "/tmp/output";
 static struct file *output_file_ptr;
 
 static int loop_open(struct inode *inode, struct file *file)
 {
     pr_info("loop_device: Device opened\n");
+
     return 0;
 }
 
@@ -29,51 +37,76 @@ static ssize_t loop_read(struct file *file, char __user *buf,
 static ssize_t loop_write(struct file *file, const char __user *buf,
                           size_t count, loff_t *ppos)
 {
-    char *hex_buffer;
-    size_t i, j;
-    ssize_t bytes_written = 0;
-    int rows = count / BYTES_PER_ROW;
-    int remaining = count % BYTES_PER_ROW;
+    // char *hex_buffer;
+    // size_t i, j;
+    // ssize_t bytes_written = 0;
+    // int rows = count / BYTES_PER_ROW;
+    // int remaining = count % BYTES_PER_ROW;
 
-    hex_buffer = kmalloc(BYTES_PER_ROW * 3 + 2, GFP_KERNEL);
-    if (!hex_buffer)
-        return -ENOMEM;
+    // // Open the output file for writing
+    // output_file_ptr = filp_open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    // if (IS_ERR(output_file_ptr)) 
+    // {
+    //     pr_err("loop_device: Unable to open output file\n");
+    //     unregister_chrdev(LOOP_MAJOR, LOOP_NAME);
+    //     return PTR_ERR(output_file_ptr);
+    // }
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < BYTES_PER_ROW; j++) {
-            sprintf(&hex_buffer[j * 3], "%02X ", buf[i * BYTES_PER_ROW + j] & 0xFF);
-        }
-        hex_buffer[BYTES_PER_ROW * 3] = '\n';
-        hex_buffer[BYTES_PER_ROW * 3 + 1] = '\0';
+    // pr_info("loop_device: %s file opend.\n", output_file);
 
-        if (ppos)
-            if (*ppos + bytes_written + count > i * BYTES_PER_ROW)
-                break;
 
-        pr_info("%s", hex_buffer);
+    pr_info("loop_device: %s\n", buf);
 
-        // Write to the output file
-        kernel_write(output_file_ptr, hex_buffer, BYTES_PER_ROW * 3 + 1, &output_file_ptr->f_pos);
-        bytes_written += BYTES_PER_ROW;
-    }
+    // hex_buffer = kmalloc(BYTES_PER_ROW * 3 + 2, GFP_KERNEL);
+    // if (!hex_buffer)
+    //     return -ENOMEM;
 
-    if (remaining > 0) {
-        for (j = 0; j < remaining; j++) {
-            sprintf(&hex_buffer[j * 3], "%02X ", buf[i * BYTES_PER_ROW + j] & 0xFF);
-        }
-        hex_buffer[remaining * 3] = '\n';
-        hex_buffer[remaining * 3 + 1] = '\0';
+    // for (i = 0; i < rows; i++) {
+    //     for (j = 0; j < BYTES_PER_ROW; j++) {
+    //         sprintf(&hex_buffer[j * 3], "%02X ", buf[i * BYTES_PER_ROW + j] & 0xFF);
+    //     }
+    //     hex_buffer[BYTES_PER_ROW * 3] = '\n';
+    //     hex_buffer[BYTES_PER_ROW * 3 + 1] = '\0';
 
-        pr_info("%s", hex_buffer);
+    //     if (ppos)
+    //         if (*ppos + bytes_written + count > i * BYTES_PER_ROW)
+    //             break;
 
-        // Write to the output file
-        kernel_write(output_file_ptr, hex_buffer, remaining * 3 + 1, &output_file_ptr->f_pos);
-        bytes_written += remaining;
-    }
+    //     pr_info("%s", hex_buffer);
 
-    kfree(hex_buffer);
+    //     // Write to the output file
+    //     kernel_write(output_file_ptr, hex_buffer, BYTES_PER_ROW * 3 + 1, &output_file_ptr->f_pos);
+    //     bytes_written += BYTES_PER_ROW;
+    // }
 
-    return bytes_written;
+    // if (remaining > 0) {
+    //     for (j = 0; j < remaining; j++) {
+    //         sprintf(&hex_buffer[j * 3], "%02X ", buf[i * BYTES_PER_ROW + j] & 0xFF);
+    //     }
+    //     hex_buffer[remaining * 3] = '\n';
+    //     hex_buffer[remaining * 3 + 1] = '\0';
+
+    //     pr_info("%s", hex_buffer);
+
+    //     // Write to the output file
+    //     kernel_write(output_file_ptr, hex_buffer, remaining * 3 + 1, &output_file_ptr->f_pos);
+    //     bytes_written += remaining;
+    // }
+
+    // kfree(hex_buffer);
+
+
+    // Close the output file
+    // if (output_file_ptr) 
+    // {
+    //     filp_close(output_file_ptr, NULL);
+
+    //     pr_info("loop_device: %s file closed.\n", output_file);
+    // }
+
+    // return bytes_written;
+
+    return 0;
 }
 
 static struct file_operations loop_fops = {
@@ -85,34 +118,46 @@ static struct file_operations loop_fops = {
 
 static int __init loop_init(void)
 {
+    dev_t dev;
     int ret;
 
-    ret = register_chrdev(LOOP_MAJOR, LOOP_NAME, &loop_fops);
-    if (ret < 0) {
-        pr_err("loop_device: Unable to register character device\n");
+    // Request a dynamic major number from the kernel for our device
+    ret = alloc_chrdev_region(&dev, 0, 1, LOOP_NAME);
+
+    if (ret < 0) 
+    {
+        printk(KERN_ERR "Failed to allocate major number\n");
         return ret;
     }
 
-    // Open the output file for writing
-    output_file_ptr = filp_open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (IS_ERR(output_file_ptr)) {
-        pr_err("loop_device: Unable to open output file\n");
-        unregister_chrdev(LOOP_MAJOR, LOOP_NAME);
-        return PTR_ERR(output_file_ptr);
+    // Extract the major number from the device identifier (dev)
+    major = MAJOR(dev);
+
+    printk(KERN_INFO "My Driver: Registered with major number %d\n", major);
+
+    // Initialize the character device structure and associate it with file operations
+    cdev_init(&my_cdev, &loop_fops);
+    my_cdev.owner = THIS_MODULE;
+
+    // Add the character device to the kernel
+    ret = cdev_add(&my_cdev, dev, 1);
+    if (ret < 0) 
+    {
+        printk(KERN_ERR "Failed to add character device\n");
+        unregister_chrdev_region(dev, 1);
+        return ret;
     }
 
-    pr_info("loop_device: Device created with major %d\n", LOOP_MAJOR);
-    return 0;
+    return 0; // Return 0 on success
 }
 
 static void __exit loop_exit(void)
 {
-    // Close the output file
-    if (output_file_ptr) {
-        filp_close(output_file_ptr, NULL);
-    }
+    // unregister_chrdev(LOOP_MAJOR, LOOP_NAME);
 
-    unregister_chrdev(LOOP_MAJOR, LOOP_NAME);
+    // Unregister the major number when the module is unloaded
+    unregister_chrdev_region(MKDEV(major, 0), 1);
+    
     pr_info("loop_device: Device removed\n");
 }
 
