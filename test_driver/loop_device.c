3ed50545ce00a8e0b953fe4c1c1ea6746aca2f87
Mikayel Smbatyan <asd@jduo.az>
2023-08-10 00:40:04 +0400

    Implemented for strings of length aligned to 16.

--- test_driver/loop_device.c
+++ test_driver/loop_device.c
@@ -39,17 +39,33 @@ static ssize_t loop_read(struct file *file, char __user *buf,
     return 0;
 }
 
-static ssize_t loop_write(struct file *file, const char __user *buf,
+static ssize_t loop_write(struct file *file, const char __user *usr_buf,
                           size_t count, loff_t *ppos)
 {
-    pr_info("loop_device: Device writing.\n");
 
     char *hex_buffer;
     size_t i, j;
     ssize_t bytes_written = 0;
     int rows = count / BYTES_PER_ROW;
     int remaining = count % BYTES_PER_ROW;
+    const char *table = "0123456789ABCDEF";
+    char* buf = NULL;
+    pr_info("loop_device: Device writing.\n");
 
+    buf = kmalloc(count, GFP_KERNEL);
+    if (!buf) {
+        pr_err("loop_device: LAVD QUNEM\n");
+        return 0;
+    }
+
+    if (copy_from_user(buf, usr_buf, count)) {
+        pr_err("loop_device: VORD QUNEM\n");
+	kfree(buf);
+        return 0; // Failed to copy data from user space
+    }
+
+    pr_info("loop_device: buf %s \n", buf);
+    
     // Open the output file for writing
     output_file_ptr = filp_open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (IS_ERR(output_file_ptr)) 
@@ -59,16 +75,30 @@ static ssize_t loop_write(struct file *file, const char __user *buf,
         return PTR_ERR(output_file_ptr);
     }
 
-    pr_info("loop_device: %s file opend.\n", output_file);
+    pr_info("loop_device: %s file opened.\n", output_file);
 
 
-    hex_buffer = kmalloc(BYTES_PER_ROW * 3 + 2, GFP_KERNEL);
+    hex_buffer = kmalloc(BYTES_PER_ROW * 3 + 2 + 1, GFP_KERNEL);
     if (!hex_buffer)
         return -ENOMEM;
 
+    memset(hex_buffer, 0, BYTES_PER_ROW * 3 + 2);
+
     for (i = 0; i < rows; i++) {
         for (j = 0; j < BYTES_PER_ROW; j++) {
-            sprintf(&hex_buffer[j * 3], "%02X ", buf[i * BYTES_PER_ROW + j] & 0xFF);
+            char symbol;
+	    pr_info("loop_device: count %ld, %ld < %d \n", count, i, rows);
+            symbol = buf[i * BYTES_PER_ROW + j] & 0xFF;
+	    pr_info("loop_device: 1++++++++.\n");
+            //sprintf(&hex_buffer[j * 3], "%02X", buf[i * BYTES_PER_ROW + j] & 0xFF);
+	    //hex_buffer[j*3 + 2] = ' ';
+	    
+	    hex_buffer[j * 3 + 1] = table[symbol % 16];
+	    symbol /= 16;
+	    hex_buffer[j * 3] = table[symbol % 16];
+	    hex_buffer[j * 3 + 2] = ' ';
+	    pr_info("hex_buffer %s\n", hex_buffer);
+	    pr_info("loop_device: 2++++++++.\n");
         }
         hex_buffer[BYTES_PER_ROW * 3] = '\n';
         hex_buffer[BYTES_PER_ROW * 3 + 1] = '\0';
@@ -79,10 +109,11 @@ static ssize_t loop_write(struct file *file, const char __user *buf,
 
         pr_info("%s", hex_buffer);
 
-        // Write to the output file
-        kernel_write(output_file_ptr, hex_buffer, BYTES_PER_ROW * 3 + 1, &output_file_ptr->f_pos);
-        bytes_written += BYTES_PER_ROW;
     }
+   
+    // Write to the output file
+    kernel_write(output_file_ptr, hex_buffer, (BYTES_PER_ROW * 3 + 2) * rows, &output_file_ptr->f_pos);
+    bytes_written += BYTES_PER_ROW * rows;
 
     if (remaining > 0) {
         for (j = 0; j < remaining; j++) {
@@ -109,31 +140,8 @@ static ssize_t loop_write(struct file *file, const char __user *buf,
         pr_info("loop_device: %s file closed.\n", output_file);
     }
 
-    return bytes_written;
-
-
-    // pr_info("loop_device: Device writing.\n");
-    
-    // // Check if the incoming data will exceed the buffer size.
-    // if (output_buffer_size + count > sizeof(output_buffer)) 
-    // {
-    //     pr_err("loop_device: Write buffer overflow.\n");
-    //     return -ENOSPC; // Not enough space left in the buffer.
-    // }
-
-    // // Copy the data from the user buffer to the driver's buffer.
-    // if (copy_from_user(output_buffer + output_buffer_size, buf, count))
-    // {
-    //     pr_err("loop_device: Failed to copy data from user buffer.\n");
-    //     return -EFAULT; // Failed to copy data from user space.
-    // }
-
-    // output_buffer_size += count;
-
-    // pr_info("loop_device: %s.\n", output_buffer);
-
-    // return count; // Return the number of bytes written.
-
+    //return bytes_written;
+    return count;
 }
 
 static struct file_operations loop_fops = {
