0ee21975fd3efb99bcc025f034270732b37d0357
Khachatur Avetisyan <72801992+KhachaturAvetisyan@users.noreply.github.com>
2023-08-11 18:45:13 +0400

    change main logic in c_test.c

--- c_test.c
+++ c_test.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BYTES_PER_ROW   16U
-#define OFFSET_SIZE     7U
+#define BYTES_PER_ROW       16U
+#define OFFSET_SIZE         7U
+#define SYMBOLS_PER_ROW     (OFFSET_SIZE + 1 + BYTES_PER_ROW / 2 * 5)
+
 
 void dec_to_hex(char *buff, size_t dec_num, uint8_t symbols_count)
 {
@@ -19,28 +21,49 @@ void dec_to_hex(char *buff, size_t dec_num, uint8_t symbols_count)
 
 int main(void)
 {
-
-    const char *buff = "abcdabcdabcdabcd";
+    const char *buff = "abcdabcdabcdabcd\n";
     size_t buff_size = strlen(buff);
 
-    size_t rows = buff_size / BYTES_PER_ROW;
-    size_t remaining = buff_size % BYTES_PER_ROW;
+    size_t rows = (buff_size + BYTES_PER_ROW - 1) / BYTES_PER_ROW;
 
-    size_t offset = 16;
-    char *output_buff = malloc((rows + 2) * OFFSET_SIZE + (buff_size * 2) + );
+    size_t offset = 0;
+    size_t output_buff_size = rows * SYMBOLS_PER_ROW + OFFSET_SIZE + 1;
 
-    dec_to_hex(output_buff, offset, 7);
+    printf("%ld %ld\n", buff_size, output_buff_size);
 
-    printf("%.7s\n", output_buff);
+    char *output_buff = malloc(output_buff_size);
 
+    uintptr_t buf_iter = 0;
+    size_t i, j;
+    for (i = 0; i < rows; ++i)
+    {
+        dec_to_hex(&output_buff[buf_iter], offset, OFFSET_SIZE);
+        buf_iter += OFFSET_SIZE;
 
-//    for (size_t i = 0; i < rows; ++i)
-//    {
-//
-//        for (size_t j = 0; j < BYTES_PER_ROW; ++j)
-//        {
-//
-//        }
-//    }
+        output_buff[buf_iter++] = ' ';
 
+        j = 0;
+        while (j < BYTES_PER_ROW)
+        {
+            if (i * BYTES_PER_ROW + j > buff_size)
+            {
+//                buf_iter +=
+                break;
+            }
+            else
+            {
+                dec_to_hex(&output_buff[buf_iter + 2], buff[i * BYTES_PER_ROW + j], 2);
+                dec_to_hex(&output_buff[buf_iter], buff[i * BYTES_PER_ROW + j + 1], 2);
+                buf_iter += 4;
+                output_buff[buf_iter++] = j != BYTES_PER_ROW - 2 ? ' ' : '\n';
+            }
+            j += 2;
+        }
+        offset += j;
+    }
+    dec_to_hex(&output_buff[buf_iter], offset, OFFSET_SIZE);
+    buf_iter += OFFSET_SIZE;
+    output_buff[buf_iter++] = '\n';
+
+    printf("%.104s\n", output_buff);
 }
